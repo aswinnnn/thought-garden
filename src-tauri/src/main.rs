d00a4730ecc113bf3cde22bd7b0425464c881cd1
aswinnnn <aswinsnair028@gmail.com>
2024-06-01 12:19:10 +0530

    not even initial version

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,87 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use std::thread::{self, spawn};
+use tauri::api::dialog::FileDialogBuilder;
+use tauri::{scope::ipc::RemoteDomainAccessScope, Manager};
+use tauri::{window, Window};
+use tokio::runtime::Runtime;
+
+use tg_backend;
+
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+#[tauri::command]
+fn opendialog() {
+    FileDialogBuilder::new().pick_folder(|folder_path| {
+        if let Some(path) = folder_path {
+            println!("{}", path.display());
+        } else {
+            println!("Go back!");
+        }
+    });
+}
+
+#[tauri::command]
+async fn start_backend(app: tauri::AppHandle) -> Result<(), ()> {
+    // if let Some(win) = get_main_window(app).await {
+    //     win.emit("redirect", "intro")
+    //         .expect("failed to redirect to /intro");
+    // }
+    tg_backend::start().await;
+
+    Ok(())
+}
+
+#[tauri::command]
+async fn checkconfig(app: tauri::AppHandle) -> bool {
+    let configfound = true;
+    configfound
+    // if let Some(main_win) = app.get_window("main") {
+    //     if !configfound {
+    //         let to = "intro";
+    //         let _ = main_win.eval(&format!(
+    //             "window.location.replace('http://localhost:{}/{}')",
+    //             "3000", to
+    //         ));
+    //         println!("[TG-BACKEND] redirected to {}", to)
+    //     } else {
+    //         let to = "create";
+    //         let _ = main_win.eval(&format!(
+    //             "window.location.replace('http://localhost:{}/{}')",
+    //             "3000", to
+    //         ));
+    //         println!("[TG-BACKEND] redirected to {}", to)
+    //     }
+    // }
+
+}
+
+
+#[tauri::command]
+async fn redirect(to: String, app: tauri::AppHandle) {
+    if let Some(main_win) = app.get_window("main") {
+        let _ = main_win.eval(&format!(
+            "window.location.replace('http://localhost:{}/{}')",
+            "3000", to
+        ));
+        println!("[TG-BACKEND] redirected to {}", to)
+    }
+}
+
+fn main() {
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![
+            opendialog,
+            checkconfig,
+            start_backend,
+            redirect
+        ])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
+
+async fn get_main_window(app: tauri::AppHandle) -> Option<Window> {
+    if let Some(main) = app.get_window("main") {
+        return Some(main);
+    }
+    None
+}
