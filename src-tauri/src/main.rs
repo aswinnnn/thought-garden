0874e218a20135d3b5f832030d91c659467ee941
aswinnnn <aswinsnair028@gmail.com>
2024-07-12 20:56:32 +0530

    add editing feature

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,10 +2,11 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 use std::process::exit;
 use std::thread::{self, spawn};
+use std::time::Duration;
 use tauri::api::dialog::FileDialogBuilder;
 use tauri::{scope::ipc::RemoteDomainAccessScope, Manager};
 use tauri::{window, Window};
-use tg_backend::journal::Journal;
+use tg_backend::journal::{Journal, Media};
 use tokio::runtime::Runtime;
 
 use tg_backend;
@@ -121,23 +122,55 @@ async fn createconfig(app: tauri::AppHandle) {
 }
 
 #[tauri::command]
-async fn fill_post(postId: String) {
+async fn fill_post(postId: String, app: tauri::AppHandle) {
     let uuid = uuid::Uuid::parse_str(&postId).expect("uuid parsing failed");
 
     let j = match Journal::init(uuid.into_bytes().to_vec()) {
-    Ok(o) => o,
-    Err(e) => {eprintln!("[FILL-POST] {e}"); exit(1);},
+        Ok(o) => o,
+        Err(e) => {
+            eprintln!("[TG-BACKEND](FILL-POST) {e}");
+            exit(1);
+        }
     };
+
+    if let Some(main_win) = app.get_window("main") {
+        // go to create page
+        // activate the listener for fill_post
+        match main_win.eval(&format!("document.querySelector('#two').click();loadlisteners();")) {
+            Ok(_) => {println!("\x1b[32m[TG-BACKEND](FILL-POST)\x1b[0m clicked create, activated listener")},
+            Err(e) => {
+                eprintln!("\x1b[31m[TG-BACKEND](FILL-POST)\x1b[0m {e}")
+            },
+        }
+        
+        println!("\x1b[32m[TG-BACKEND](FILL-POST)\x1b[0m redirected to create")
+    }
+
+    tokio::time::sleep(Duration::from_millis(300)).await;
+
+    match app.emit_to("main", "fill_post", j) {
+    Err(e) => eprintln!("\x1b[31m[TG-BACKEND] (fill-post)\x1b[0m{e}"),
+    Ok(_) => {println!("\x1b[32m[TG-BACKEND](FILL-POST)\x1b[0m successfully emited 'fill_post'")},
+    }
+    let wallp = match Media::get(uuid.as_bytes().to_vec(), tg_backend::journal::MediaType::Wallpaper(String::new())) {
+    tg_backend::journal::MediaType::Wallpaper(w) => w,
+    _ => "unreachable".into()
+    };
+
+    println!("wallpaper: {wallp}");
+        
+    
+
+    match app.emit_to("main", "change_style", wallp) {
+    Err(e) => eprintln!("\x1b[31m[TG-BACKEND] (fill-post)\x1b[0m{e}"),
+    Ok(_) => {println!("\x1b[32m[TG-BACKEND](FILL-POST)\x1b[0m successfully emited 'change_style'")},
+    }
 
     // redirect to create
     // change wallpaper, font
     // fill date, title, and content
     // make it look nicer
-
-
 }
-#[tauri::command]
-async fn importconfig() {}
 
 fn main() {
     tauri::Builder::default()
@@ -145,12 +178,12 @@ fn main() {
             opendialog,
             checkconfig,
             createconfig,
-            importconfig,
             update_buffer,
             new_journal,
             start_backend,
             redirect,
-            call_js
+            call_js,
+            fill_post,
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
---
+++ src/create/backendstuff.js
@@ -0,0 +1,37 @@
+// import { emit, listen } from '@tauri-apps/api/event'
+// const { listen } = require('@tauri-apps/api/event');
+
+async function loadlisteners() {
+    try {
+        
+    await window.__TAURI__.event.listen('fill_post', (event) => {
+        let title = document.querySelector('.article-title');
+        let content = document.querySelector('.article-content');
+        let date = document.querySelector('.date');
+
+        console.log(event.payload)
+
+        title.innerHTML = event.payload.buffer_title;
+        content.innerText = event.payload.buffer;
+        date.innerText = event.payload.metadata.created_at;
+    })
+
+    
+    await window.__TAURI__.event.listen('change_style', (event) => {
+    document.querySelector('body').setAttribute('style','background-image: url(\''+ event.payload + '\')')
+    })
+
+    // remove/fade background when home
+    // input date through metadata
+    // perfomance issues
+
+    console.log('listeners active')
+    } catch (error) {
+        console.error("[fill_post_listener] ", error)   
+    }
+
+}
+// emits the `click` event with the object payload
+// emit('click', {
+//   theMessage: 'Tauri is awesome!',
+// })
